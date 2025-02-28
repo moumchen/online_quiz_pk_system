61a0550ad857e30dc39dbf43a3ee2df8c5ee7f8b
Yanqian Chen <270595725@yoobeestudent.ac.nz>
2025-02-28 20:44:01 +1300

    feat: create an urls file in different module

--- online_quiz_pk_system/urls.py
+++ online_quiz_pk_system/urls.py
@@ -15,8 +15,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # include all urls of the multiplayer module urls
+    path('multiplayer/', include('multiplayer.urls')),
+
+    # include all urls of the singleplayer module urls
+    path('singleplayer/', include('singleplayer.urls')),
+
+    # include all urls of the common module urls
+    path('common/', include('common.urls')),
+
 ]
