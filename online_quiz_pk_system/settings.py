dc32b457263d3bdbfa8e24291cff4b7d8452d6f2
Yanqian Chen <270595725@yoobeestudent.ac.nz>
2025-02-28 21:12:46 +1300

    feat: update settings.py

--- online_quiz_pk_system/settings.py
+++ online_quiz_pk_system/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'singleplayer',
+    'multiplayer',
+    'common'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +57,7 @@ ROOT_URLCONF = 'online_quiz_pk_system.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
