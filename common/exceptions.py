cb59eea700e2e85aef3264ebb5df3c16b3b814e4
Yanqian Chen <270595725@yoobeestudent.ac.nz>
2025-03-03 15:43:02 +1300

    feat: add a custom exception

---
+++ common/exceptions.py
@@ -0,0 +1,10 @@
+
+class ConfigNotFoundError(Exception):
+    ''' This custom exception will be used when the config is not defined in database '''
+    def __init__(self, key, message="Config key not found"):
+        self.key = key
+        self.message = f"{message}: {key}"
+        super().__init__(self.message)
+
+    def __str__(self):
+        return self.message
