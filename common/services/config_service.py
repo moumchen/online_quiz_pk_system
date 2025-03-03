d5e644b1f2331c586b9710e514e65a0252f17955
Yanqian Chen <270595725@yoobeestudent.ac.nz>
2025-03-03 15:43:02 +1300

    feat: add a custom exception

--- common/services/config_service.py
+++ common/services/config_service.py
@@ -1,7 +1,8 @@
 from ..models import Config
+from ..exceptions import ConfigNotFoundError
 
 def get_cached_config_by_key(key):
     config = Config.objects.filter(key__exact=key).first()
     if config is None:
-        raise RuntimeError("Config doesn't exist")
+        raise ConfigNotFoundError(key)
     return config
